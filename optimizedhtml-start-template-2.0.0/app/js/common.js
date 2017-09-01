0c78c16349d62e8477c32260f5b41668b95153a8
Vlad Kalensky <Damlik74@gmail.com>
2017-09-01 14:58:21 +0500

    First commit

---
+++ optimizedhtml-start-template-2.0.0/app/js/common.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+    
+    $('#tabs').on('init.tabs', function()
+{
+    // do something...
+});
+
+});
